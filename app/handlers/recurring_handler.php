c52bf8961ea79833b20a2bd035010bdca93564fa
kaka23223 <karin.ytpremium1@gmail.com>
2026-04-02 11:01:29 +0700

    Add files via upload

---
+++ app/handlers/recurring_handler.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../app/config/database.php';
+require_once '../app/core/Response.php';
+require_once '../app/models/RecurringModel.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+$database = new Database();
+$db = $database->getConnection();
+$model = new RecurringModel($db);
+
+$type = $_GET['type'] ?? ''; // 'pemasukan' atau 'pengeluaran'
+
+try {
+    if ($type === 'pemasukan') {
+        $model->createRecurringPemasukan($data);
+        Response::success(201, "Pemasukan berulang berhasil diatur.");
+    } elseif ($type === 'pengeluaran') {
+        $model->createRecurringPengeluaran($data);
+        Response::success(201, "Pengeluaran berulang berhasil diatur.");
+    } else {
+        Response::error(400, "Tipe transaksi berulang tidak valid.");
+    }
+} catch (Exception $e) {
+    Response::error(500, "Gagal menyimpan: " . $e->getMessage());
+}
