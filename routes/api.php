605fa577a27415425a5c9af41db91f53ec51b980
kaka23223 <karin.ytpremium1@gmail.com>
2026-04-02 13:31:38 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
-// 1. Tambahkan Header CORS di baris paling atas
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-// 2. Tangani request OPTIONS (Preflight dari browser Vite)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
 
-// 3. Perbaiki Logika Routing agar mengabaikan folder XAMPP
+require_once '../app/core/Response.php';
+
+// Menangkap semua error ringan (Warning, Notice, Deprecated)
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    Response::error("System Error: " . $errstr . " in " . basename($errfile) . " line " . $errline, 500);
+});
+
+// Menangkap semua error berat (Fatal Error, Uncaught Exception)
+set_exception_handler(function($exception) {
+    Response::error("Server Error: " . $exception->getMessage(), 500);
+});
+
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Ekstrak hanya bagian yang dimulai dari '/api'
 $api_endpoint = '';
 if (preg_match('/\/api(\/.*)?$/', $request_uri, $matches)) {
     $api_endpoint = '/api' . ($matches[1] ?? '');
@@ -21,10 +30,8 @@ if (preg_match('/\/api(\/.*)?$/', $request_uri, $matches)) {
     $api_endpoint = $request_uri;
 }
 
-// Hapus slash di akhir jika ada agar seragam
 $api_endpoint = rtrim($api_endpoint, '/');
 
-// 4. Routing API menggunakan endpoint yang sudah dibersihkan
 switch ($api_endpoint) {
     
     case '/api/user':
@@ -56,7 +63,6 @@ switch ($api_endpoint) {
         break;
 
     default:
-        require_once '../app/core/Response.php';
-        Response::error(404, "Endpoint API tidak ditemukan. Periksa kembali URL Anda.");
+        Response::error("Endpoint API tidak ditemukan. Periksa kembali URL Anda.", 404);
         break;
 }
