163991ca9b10ef94994e0c8e290ee023ee0f1980
kaka23223 <karin.ytpremium1@gmail.com>
2026-04-08 08:22:21 +0700

    Memperbaiki register dan login dengan menambahkan fitur minimal 6 karakter untuk login dan register

--- app/controllers/OtentikasiController.php
+++ app/controllers/OtentikasiController.php
@@ -14,12 +14,29 @@ class OtentikasiController {
         $data = json_decode(file_get_contents("php://input"));
 
         if(!empty($data->username) && !empty($data->password) && !empty($data->confirm_password)) {
+            
+            // VALIDASI BARU: Cek minimal karakter username
+            if(strlen($data->username) < 6) {
+                Response::json(400, "error", "Username minimal harus terdiri dari 3 karakter!");
+                return; // Hentikan proses jika gagal validasi
+            }
+
+            // VALIDASI BARU: Cek minimal karakter password
+            if(strlen($data->password) < 6) {
+                Response::json(400, "error", "Password minimal harus terdiri dari 6 karakter!");
+                return; // Hentikan proses jika gagal validasi
+            }
+
             if($data->password !== $data->confirm_password) {
                 Response::json(400, "error", "Password dan Konfirmasi Password tidak cocok!");
+                return;
             }
+            
             if($this->userModel->isUsernameExists($data->username)) {
                 Response::json(400, "error", "Username sudah digunakan!");
+                return;
             }
+            
             if($this->userModel->register($data->username, $data->password)) {
                 Response::json(201, "success", "Registrasi berhasil! Silakan masuk.");
             } else {
